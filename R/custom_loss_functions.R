8c58fb4693cabab426ea3dd36e32f5f1c4017ca2
anibalsilva1 <anibalferreiraesilva@gmail.com>
2022-10-04 13:39:53 +0100

    Changed custom loss function logic to be called with weights as inputs.

--- R/custom_loss_functions.R
+++ R/custom_loss_functions.R
@@ -6,37 +6,62 @@
 #' @param preds Numeric \code{vector} of predictions.
 #' @param dtrain \code{xgb.DMatrix} object.
 #'
-#' @return Returns a \code{list with the following elements:
+#' @return Returns a \code{list} containing the gradient and the hessian of SERA.
+#'
+#' @references https://github.com/dmlc/xgboost/blob/master/R-package/demo/custom_objective.R
 #'
 #' @export
 #'
 #' @examples
 #'
-#' @references
-#' https://github.com/dmlc/xgboost/blob/master/R-package/demo/custom_objective.R
-#' @importFrom xgboost getinfo
-#' @importFrom IRon phi
+#' library(IRon)
+#' library(xgboost)
+#' library(dplyr)
+#'
+#' data("NO2Emissions")
+#'
+#' n <- nrow(NO2Emissions)
+#' s <- sample(1:n, size = n*0.8)
+#'
+#' formula <- LNO2 ~ .
+#' train <- NO2Emissions %>% dplyr::slice(s)
+#' test <- NO2Emissions %>% dplyr::slice(-s)
+#'
+#' train_m <- data.matrix(dplyr::select(train, -LNO2))
+#' test_m <- data.matrix(dplyr::select(test, -LNO2))
+#'
+#' labels_train <- train$LNO2
+#' labels_test <- test$LNO2
+#'
+#' I <- 1000
+#' phi <- phi(labels_train)
+#'
+#' steps <- seq(0, 1, 1/I)
+#' sigma <- sigma(phis=phi, steps=steps)
+#'
+#' xgb_dtrain <- xgb.DMatrix(data = train_m, label = labels_train, weight = sigma)
+#' xgb_dtest <- xgb.DMatrix(data = test_m, label = labels_test)
+#'
+#' params <- list(objective=custom.xgboost.sera, max_depth=5, eta=10^{-1})
+#'
+#' xgb.model <- xgboost(data=xgb_dtrain, nrounds = 100, params = params, verbose = 0)
+#' preds.xgb <- predict(xgb.model, xgb_dtest)
+#'
+
 custom.xgboost.sera <- function(preds, dtrain){
 
-  s <- 0.001
   labels <- xgboost::getinfo(dtrain, "label")
+  sigma <- xgboost::getinfo(dtrain, "weight")
+  if(is.null(sigma)) stop("Weights must be provided.")
 
-  step <- seq(0,1,s)
-
-  p.extrm <- get_extreme_type(labels)
-  phi.ctrl <- IRon::phi.control(labels, extr.type = p.extrm)
-
-  phi.trues <- IRon::phi(labels, phi.ctrl)
-  sigmas <- sigma(phis=phi.trues, steps=step)
-
-  grad <- sigmas*(preds-labels)
-  hess <- sigmas
+  grad <- sigma*(preds-labels)
+  hess <- sigma
 
   return(list(grad = grad, hess = hess))
 }
 
 
-#' Custom SERA loss function adaptation for Light GBM.
+#' Custom SERA loss function adaptation for LightGBM.
 #'
 #' @description Determines the gradient statistics of SERA to be used in
 #' LGBM algorithm as a custom loss function.
@@ -44,30 +69,50 @@ custom.xgboost.sera <- function(preds, dtrain){
 #' @param preds Numeric \code{vector} of predictions.
 #' @param dtrain A \code{lgb.Dataset} object.
 #'
-#' @return  Returns a list containing the gradient and the hessian of SERA.
+#' @return  Returns a \code{list} containing the gradient and the hessian of SERA.
 #' @export
-#' @references
-#' https://github.com/microsoft/LightGBM/blob/master/R-package/tests/testthat/test_custom_objective.R
+#' @references https://github.com/microsoft/LightGBM/blob/master/R-package/tests/testthat/test_custom_objective.R
 #'
-#' @importFrom lightgbm get_field
-#' @importFrom IRon phi.control
-#' @importFrom IRon phi
 #' @examples
+#' library(IRon)
+#' library(lightgbm)
+#' library(dplyr)
+#'
+#' data("NO2Emissions")
+#'
+#' n <- nrow(NO2Emissions)
+#' s <- sample(1:n, size = n*0.8)
+#'
+#' formula <- LNO2 ~ .
+#' train <- NO2Emissions %>% dplyr::slice(s)
+#' test <- NO2Emissions %>% dplyr::slice(-s)
+#'
+#' train_m <- data.matrix(dplyr::select(train, -LNO2))
+#' test_m <- data.matrix(dplyr::select(test, -LNO2))
+#'
+#' labels_train <- train$LNO2
+#'
+#' I <- 1000
+#' phi <- phi(labels_train)
+#'
+#' steps <- seq(0, 1, 1/I)
+#' sigma <- sigma(phis=phi, steps=steps)
+#'
+#' lgbm_dtrain <- lgb.Dataset(data = train_m, label = labels_train, weight = sigma)
+#'
+#' params <- list(objective=custom.lgbm.sera, max_depth=5, eta=10^{-1}, num_leaves=5)
+#'
+#' lgbm.model <- lightgbm(data=lgbm_dtrain, nrounds = 100, params = params, verbose = -1)
+#' preds.lgbm <- predict(lgbm.model, test_m)
+#'
 custom.lgbm.sera <- function(preds, dtrain){
 
   labels <- lightgbm::get_field(dtrain, "label")
+  sigma <- lightgbm::get_field(dtrain, "weight")
+  if(is.null(sigma)) stop("Weights must be provided.")
 
-  s <- 0.001
-  step <- seq(0,1,s)
-
-  p.extrm <- get_extreme_type(labels)
-  phi.ctrl <- IRon::phi.control(labels, extr.type = p.extrm)
-
-  phi.trues <- IRon::phi(labels, phi.ctrl)
-  sigmas <- sigma(phis=phi.trues, steps=step)
-
-  grad <- sigmas*(preds-labels)
-  hess <- sigmas
+  grad <- sigma*(preds-labels)
+  hess <- sigma
 
   return(list(grad = grad, hess = hess))
 }
